ec9319cdf6edeef571ad385756821a940f83247e
Romain Tartière <romain@blogreen.org>
2015-08-16 10:18:59 +0200

    Implement relations browsability.
    Fixes #12.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
-
+  def company_link(target)
+    if target == @account
+      target.company
+    else
+      link_to target.company, account_relations_path(target)
+    end
+  end
 end
